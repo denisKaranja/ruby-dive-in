78160f613672ec8f99ab362d5c36f3fad44a04fa
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-05-09 21:39:00 +0300

    Code: setters and getters
    Automated setters and getters. attr_writer, attr_reader and attr_accessor

--- classes.rb
+++ classes.rb
@@ -3,6 +3,12 @@ Classes
 =end
 
 class Animal
+	#getters and setters
+	attr_accessor :name
+	attr_accessor :color
+	attr_reader :legs, :arms
+	attr_writer :height
+
 	def set_noise(noise)
 		# instance variables '@'
 		@noise = noise
@@ -11,11 +17,16 @@ class Animal
 	def get_noise()
 		@noise
 	end
+
 end
 
 cow = Animal.new()
 	cow.set_noise("Moo!")
 	puts cow.get_noise()
+	cow.name = "Mooshkins"
+	puts "My name is #{cow.name}"
+	cow.color = "Black"
+	puts "And i am #{cow.color} in color"
 
 duck = Animal.new()
 	duck.set_noise("Quack!")
